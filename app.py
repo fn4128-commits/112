95a90135c8207c8e19eddf86d055aafd901be36d
fn4128-commits <fn4128@gmail.com>
2026-01-13 11:41:54 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="我的 Streamlit 网页")
+
+st.title("你好，这是网页版 Streamlit 应用")
+st.write("这个页面是部署在 Streamlit Community Cloud 上的示例。")
+st.text("你可以随便写点介绍、项目说明、作业展示等等。")
